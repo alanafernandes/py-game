eb6175709df667098fb76dcf288f6736f0f0c665
Igor Sgarbossa Detoni <116043868+igor-detoni@users.noreply.github.com>
2022-11-20 19:42:02 -0300

    Update jogo.py
    nomes dos imports arrumados

--- jogo.py
+++ jogo.py
@@ -1,7 +1,7 @@
 import pygame, random,  pygame.mixer, time
 from pygame.locals import *
-from scripts.player import Jogador, Tiro, Planeta
-from scripts.enemy import Inimigo
+from scripts.jogador import Jogador, Tiro, Planeta
+from scripts.inimigo import Inimigo
 from funcoes import limparTela, lerTexto, lerArquivo, salvarArquivo
 
 
