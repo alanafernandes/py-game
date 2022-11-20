27ce229171a07f502a28d4a157424b068c25162e
Igor Sgarbossa Detoni <116043868+igor-detoni@users.noreply.github.com>
2022-11-20 19:28:36 -0300

    Update jogo.py
    adicionados os grupos de cada objeto (jogador, inimigo, tiros e o planeta)

--- jogo.py
+++ jogo.py
@@ -1,6 +1,6 @@
 import pygame, random,  pygame.mixer, time
 from pygame.locals import *
-from scripts.player import Jogador, Tiro
+from scripts.player import Jogador, Tiro, Planeta
 from scripts.enemy import Inimigo
 from funcoes import limparTela, lerTexto, lerArquivo, salvarArquivo
 
@@ -22,9 +22,25 @@ class Game():
         self.inicio_jogo = True 
 
         self.largura = 1200 
-        self.altura = 750 
+        self.altura = 600 
 
         self.janela = pygame.display.set_mode((self.largura, self.altura)) 
 
         self.fundo = pygame.image.load("img/fundo.jpg") 
         self.fundo = pygame.transform.scale(self.fundo, (self.largura, self.altura)) 
+        
+        self.planet_group = pygame.sprite.Group()
+        self.planeta = Planeta()
+        self.planet_group.add(self.planeta)
+
+        self.grupo_jogador = pygame.sprite.Group() 
+        self.jogador = Jogador() 
+        self.grupo_jogador.add(self.jogador) 
+        self.jogador_direita = False 
+        self.jogador_esquerda = False 
+
+        self.tiros_grupo = pygame.sprite.Group() 
+
+        self.inimigo_c = True
+        self.grupo_inimigo = pygame.sprite.Group()
+
