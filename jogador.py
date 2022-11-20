d897a50f421958d907788a9d8a5ff118db2618c3
Igor Sgarbossa Detoni <116043868+igor-detoni@users.noreply.github.com>
2022-11-20 19:13:57 -0300

    Add files via upload
    pasta onde as funções que envolvem o jogador estão alocadas

---
+++ jogador.py
@@ -0,0 +1,42 @@
+import pygame
+from pygame.locals import *
+
+class Jogador (pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.imagens = [pygame.image.load("img/jogador1.png").convert_alpha(),
+                            pygame.image.load("img/jogador2.png").convert_alpha()]
+
+        self.image_agora = 0
+        self.velocidade = 20
+
+        self.image = pygame.image.load("img/jogador1.png").convert_alpha()
+        self.image = pygame.transform.scale(self.image, (61, 78))
+        self.rect = self.image.get_rect()
+        self.rect[0] = 600
+        self.rect[1] = 490
+        self.pontos = 0
+
+    def update(self):
+        self.animation()
+
+    def animation(self):
+        self.image_agora = (self.image_agora +1 ) %2
+        self.image = self.imagens[self.image_agora]
+        self.image = pygame.transform.scale(self.image, (68, 78))
+
+
+class Tiro (pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("img/tiro.png").convert_alpha()
+        self.rect = self.image.get_rect()
+        self.velocidade = 30
+
+
+    def update(self):
+        self.rect[1] -= self.velocidade
+
+
+
